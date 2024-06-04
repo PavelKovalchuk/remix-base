05835101e13c913162dc686785c49174ae86f689
pkovalchuk <pavel.kovalchuk@dataart.com>
2024-06-04 15:34:08 +0200

    feat: add base notes

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,5 +1,9 @@
-import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+import { redirect } from '@remix-run/node';
 
+import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+import { getStoredNotes, storeNotes } from '~/data/notes';
+
+// For GET request
 export default function NotesPage() {
   return (
     <main>
@@ -8,6 +12,20 @@ export default function NotesPage() {
   );
 }
 
+// Reserved function name to handle backend requests
+// For non GET requests
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  // Add validation...
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect('/notes');
+}
+
+// Reserved function name to inject styles
 export function links() {
   return [...newNoteLinks()];
 }
