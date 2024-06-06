ee24003c0502a528789f37831d2a46b780417dbf
pkovalchuk <pavel.kovalchuk@dataart.com>
2024-06-06 17:49:13 +0200

    feat: add base routing

--- app/components/NoteList.tsx
+++ app/components/NoteList.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@remix-run/react';
 import styles from './NoteList.css?url';
 
 export interface INote {
@@ -11,26 +12,28 @@ function NoteList({ notes }: { notes: INote[] }) {
     <ul id="note-list">
       {notes.map((note, index: number) => (
         <li key={note.id} className="note">
-          <article>
-            <header>
-              <ul className="note-meta">
-                <li>#{index + 1}</li>
-                <li>
-                  <time dateTime={note.id}>
-                    {new Date(note.id).toLocaleDateString('en-US', {
-                      day: 'numeric',
-                      month: 'short',
-                      year: 'numeric',
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
-                  </time>
-                </li>
-              </ul>
-              <h2>{note.title}</h2>
-            </header>
-            <p>{note.content}</p>
-          </article>
+          <Link to={note.id}>
+            <article>
+              <header>
+                <ul className="note-meta">
+                  <li>#{index + 1}</li>
+                  <li>
+                    <time dateTime={note.id}>
+                      {new Date(note.id).toLocaleDateString('en-US', {
+                        day: 'numeric',
+                        month: 'short',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </time>
+                  </li>
+                </ul>
+                <h2>{note.title}</h2>
+              </header>
+              <p>{note.content}</p>
+            </article>
+          </Link>
         </li>
       ))}
     </ul>
